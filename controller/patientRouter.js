48059f72bcbaecd3f02f4a5b5eced8cf87e31ab6
Melvinpml <melvinpoulose06@gmail.com>
2024-02-08 15:21:49 +0530

    patientBackend added

---
+++ controller/patientRouter.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const patientModel = require("../patientModel/patientModel")
+
+const router = express.Router()
+
+router.post("/add",async(req,res)=>{
+    let data = req.body
+    let patient = new patientModel(data)
+    let result = await patient.save()
+    res.json({status:"success"})
+})
+
+router.get("/viewall",async(req,res)=>{
+    let data = await patientModel.find()
+    res.json(data)
+})
+
+module.exports = router
